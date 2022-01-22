112b31f3626443520aa3c822774def2393fcb2b1
Pravesh <pravesh2655@gmail.com>
2022-01-22 12:11:16 +0530

    added something

---
+++ application/controllers/homepage_controller.php
@@ -0,0 +1,9 @@
+<?php  
+class Davagenie extends CI_Controller{
+    public function homepage(){
+        // echo "<h1>this is testing</h1>";
+        $this->load->view('davagenie_homepage');
+    }
+}
+
+?>
--- application/views/main.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Main extends CI_Controller{
-
-
-}
-
-
-?>
